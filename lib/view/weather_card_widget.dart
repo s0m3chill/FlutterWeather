86f5efaf2f3108c065df532b7a9c38414cc3d4b6
Dariy Kordiyak <d.kordiyak@gmail.com>
2018-08-23 17:18:19 +0300

    Add fadeIn weather image to UITableView

--- lib/view/weather_card_widget.dart
+++ lib/view/weather_card_widget.dart
@@ -18,7 +18,8 @@ class WeatherCardWidget extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.network('https://openweathermap.org/img/w/${weather.icon}.png'),
+            FadeInImage.assetNetwork(placeholder: '',
+                image: 'https://openweathermap.org/img/w/${weather.icon}.png'),
             Text('${weather.main}, ${weather.temp.toInt().toString()}°C', style: new TextStyle(color: Colors.white, fontSize: 30.0)),
             Text(new DateFormat.yMMMd().format(weather.date), style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic)),
             Text(new DateFormat.Hm().format(weather.date), style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic)),
