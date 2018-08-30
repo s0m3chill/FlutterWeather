585ad368505d698f277d276f7d3f03e8b7afec5e
Dariy Kordiyak <d.kordiyak@gmail.com>
2018-08-30 11:32:58 +0300

    WeatherWidget stateful

--- lib/view/weather_widget.dart
+++ lib/view/weather_widget.dart
@@ -3,11 +3,21 @@ import 'package:flutter_weather/model/weather_data.dart';
 
 import 'package:intl/intl.dart';
 
-class WeatherWidget extends StatelessWidget {
+class WeatherWidget extends StatefulWidget {
+
+  const WeatherWidget({Key key, this.weather}) : super(key: key);
 
   final WeatherData weather;
 
-  WeatherWidget({Key key, @required this.weather}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return WeatherWidgetState();
+  }
+}
+
+class WeatherWidgetState extends State<WeatherWidget> {
+
+  //WeatherWidget({Key key, @required this.weather}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +25,20 @@ class WeatherWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         FadeInImage.assetNetwork(placeholder: '',
-            image: 'https://openweathermap.org/img/w/${weather.icon}.png'),
-        Text('${weather.main}, ${weather.temp.toInt().toString()}°C', style: new TextStyle(color: Colors.white, fontSize: 30.0)),
+            image: 'https://openweathermap.org/img/w/${widget.weather.icon}.png'),
+        Text('${widget.weather.main}, ${widget.weather.temp.toInt().toString()}°C', style: new TextStyle(color: Colors.white, fontSize: 30.0)),
         RichText(
           text: new TextSpan(
             style: new TextStyle(
               fontSize: 14.0,
             ),
             children: <TextSpan>[
-              new TextSpan(text: '${weather.name}', style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.white)),
+              new TextSpan(text: '${widget.weather.name}', style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.white)),
             ],
           ),
         ),
-        Text(new DateFormat.yMMMd().format(weather.date), style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic)),
-        Text(new DateFormat.Hm().format(weather.date), style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic)),
+        Text(new DateFormat.yMMMd().format(widget.weather.date), style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic)),
+        Text(new DateFormat.Hm().format(widget.weather.date), style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic)),
       ],
     );
   }
